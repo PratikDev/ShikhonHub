061330b3cf8659ead2d04a8c7f6cee828c492d29
Pratik Dev <87067570+PratikDev@users.noreply.github.com>
2024-12-25 16:19:50 +0000

    removed theme toggler
    facing some issues rn

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -30,27 +30,25 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex gap-3 h-14 items-center justify-end mx-auto px-2">
-        <div className="mr-4 hidden md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
-            <span className="hidden font-bold sm:inline-block text-primary">
-              ShikhonHub
-            </span>
-          </Link>
-          <nav className="flex items-center space-x-6 text-sm font-medium">
-            {navItems.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="transition-colors hover:text-primary text-muted-foreground"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </nav>
-        </div>
+      <div className="container flex gap-x-12 h-14 items-center mx-auto px-2">
+        <Link href="/" className="flex items-center">
+          <span className="font-bold text-primary">
+            ShikhonHub
+          </span>
+        </Link>
+        <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
+          {navItems.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="transition-colors hover:text-primary text-muted-foreground"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
 
-        <div className="flex md:flex-1 items-center justify-between space-x-2 md:justify-end">
+        {/* <div className="flex md:flex-1 items-center justify-between space-x-2 md:justify-end">
           <nav className="flex items-center">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -73,20 +71,21 @@ export function Header() {
               </DropdownMenuContent>
             </DropdownMenu>
           </nav>
-        </div>
+        </div> */}
 
         <Sheet>
           <SheetTrigger asChild>
             <Button
               variant="ghost"
-              className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+              className="ml-auto px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
             >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle Menu</span>
             </Button>
           </SheetTrigger>
+          
           <SheetContent side="left" className="pr-0">
-            <Link href="/" className="flex items-center" onClick={() => {}}>
+            <Link href="/" className="flex items-center" onClick={() => { }}>
               <span className="font-bold text-primary">ShikhonHub</span>
             </Link>
             <nav className="flex flex-col gap-4 mt-4">
